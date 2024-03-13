2d2604e8a71cdc17bb8b96513849928295f640a2
Khirasagar <khirasagar.maharana8100@gmail.com>
2024-03-13 10:34:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ function locomotiveAnimation() {
 
     // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
 
-
     const locoScroll = new LocomotiveScroll({
         el: document.querySelector("#main"),
         smooth: true
